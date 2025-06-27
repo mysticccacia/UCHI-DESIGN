6777c993892a4c9cc8abbbaf857520e3f5e54472
Abhishek Kushwaha <152796449+mysticcasia@users.noreply.github.com>
2025-06-27 10:28:35 +0530

    Added app.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,32 +3,34 @@ import Eyes from "./Eyes";
 import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 
 const Projects = () => {
-
   return (
-    <div className='w-full min-h-screen pb-[2vw] text-zinc-900 bg-[#CEEA67] overflow-hidden '>
-            <div data-scroll  data-scroll-speed="-.7" className='relative w-full flex flex-col items-center justify-center h-full font-["FoundersGrotesk, Roboto, sans-serif"]'>
-              <div className="absolute gap-[3vw] top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] flex justify-between">
-              <Eyes/>   
-              </div>
-                <h1 className='text-[12vw] leading-none font-bold -mb-4 tracking-tighter'>READY</h1>
-                <h1 className='text-[12vw] leading-none font-bold -mb-4  tracking-tighter'>TO START</h1>
-                <h1 className='text-[12vw] leading-none font-bold -mb-4  tracking-tighter'>THE PROJECT ?</h1>
-                {/* <div className="mt-[2vw] bg-[#212121] rounded-full px-[1vw] py-[1vw]  w-fit flex justify-between items-center gap-[1.5vw] text-white ">
-                <span className="tracking-tight mr-1">START THE PROJECT</span>
-                <span className="bg-white text-black p-[0.5vw] rounded-full">
-                  <ArrowOutwardIcon />
-                </span> */}
-                {/* </div>
-                <h1 className="mt-[1vw] font-regular text-xl">OR</h1>
-                 <div className="mt-[2vw] bg-transparent border-1 rounded-full px-[1vw] py-[1vw]  w-fit flex justify-between items-center gap-[1.5vw] text-zinc-900 ">
-                <span className="tracking-tight ">HELLO@UCHI.DESIGN</span>
-                <span className="bg-black text-white p-[0.5vw] rounded-full">
-                  <ArrowOutwardIcon />
-                </span>
-                </div> */}
-            </div>
+    <div className="w-full min-h-screen pb-[2vw] text-zinc-900 bg-[#CEEA67] overflow-hidden ">
+      <div
+        data-scroll
+        data-scroll-speed="-.7"
+        className='relative w-full flex flex-col items-center justify-center h-full font-["FoundersGrotesk, Roboto, sans-serif"]'
+      >
+        <div className="absolute gap-[3vw] top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] flex justify-between">
+          <Eyes />
+        </div>
+        <h1 className="text-[12vw] leading-none font-bold -mb-4 tracking-tighter">
+          READY
+        </h1>
+        <h1 className="text-[12vw] leading-none font-bold -mb-4  tracking-tighter">
+          TO START
+        </h1>
+        <h1 className="text-[12vw] leading-none font-bold -mb-4  tracking-tighter">
+          THE PROJECT ?
+        </h1>
+        <div className="mt-[2vw] bg-[#212121] rounded-full px-[1vw] py-[1vw]  w-fit flex justify-between items-center gap-[1.5vw] text-white ">
+          <span className="tracking-tight mr-1">START THE PROJECT</span>
+          <span className="bg-white text-black p-[0.5vw] rounded-full">
+            <ArrowOutwardIcon />
+          </span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects 
+export default Projects;
